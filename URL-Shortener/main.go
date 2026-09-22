4682ec7c3c34959639b326a5d31ca4a11ca52492
khaledmahmudriad <130629420+khaledmahmudriad@users.noreply.github.com>
2026-09-22 21:59:36 +0600

    Add URL data structure and storage

--- URL-Shortener/main.go
+++ URL-Shortener/main.go
@@ -5,13 +5,19 @@ import (
 	"net/http"
 )
 
-func main() {
+// Link stores information about one URL
+type Link struct {
+	ID      int
+	LongURL string
+}
 
+// This slice will temporarily store our URLs
+var links []Link
+
+func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "URL Shortener")
 	})
-
 	fmt.Println("Server running on http://localhost:8080")
-
 	http.ListenAndServe(":8080", nil)
 }
